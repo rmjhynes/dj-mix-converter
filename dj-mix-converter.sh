8b1472011fbb717c3da9af222c37914d0c3db990
Ryan Hynes <131348960+rmjhynes@users.noreply.github.com>
2025-11-09 14:24:46 +0000

    chore: formatting

--- dj-mix-converter.sh
+++ dj-mix-converter.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Location of .wav files saved from Rekordbox
-TARGET_DIR="$HOME/Music/rekordbox/Recording"
-OUTPUT_DIR="$TARGET_DIR/mp3"
+TARGET_DIR="${HOME}/Music/rekordbox/Recording"
+OUTPUT_DIR="${TARGET_DIR}/mp3"
 mkdir -p "$OUTPUT_DIR"
 
 # Create logs and output directories
@@ -11,24 +11,29 @@ mkdir -p logs
 LOG_FILE="logs/mix_conversion_${TIMESTAMP}.log"
 
 # Create log file header
-echo "DJ Mix Conversion Log - $(date)" > "$LOG_FILE"
-echo "==========================================" >> "$LOG_FILE"
-echo "" >> "$LOG_FILE"
+{
+  echo "DJ Mix Conversion Log - $(date)"
+  echo "=========================================="
+  echo ""
+} > "$LOG_FILE"
 
 # Find all .wav files in target directory and remove the file extension
 WAV_FILES=$(find $TARGET_DIR -type f -name '*.wav' | cut -f 1 -d '.')
 
 # Iterate over list of files and convert to .mp3
 for file in $WAV_FILES; do
+
   # Get file name from absolute path
   name=$(basename "$file")
 
   # Convert to mp3 with ffmpeg and tee output to log file
   ffmpeg -n -i "$file.wav" -codec:a libmp3lame -b:a 320k "$OUTPUT_DIR/${name}.mp3" 2>&1 | tee -a "$LOG_FILE"
 
-  echo "" >> "$LOG_FILE"
-  echo "==========================================" >> "$LOG_FILE"
-  echo "" >> "$LOG_FILE"
+  {
+    echo ""
+    echo "=========================================="
+    echo ""
+  } >> "$LOG_FILE"
 done
 
 echo "File(s) converted and stored in $OUTPUT_DIR"
